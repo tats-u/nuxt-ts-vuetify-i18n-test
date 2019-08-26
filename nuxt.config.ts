1d11372e19cc759e2d844e3bbbddbac515ad73a2
Tatsunori Uchino <tats.u@live.jp>
2019-08-26 16:00:36 +0900

    Enable nuxt-i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,18 +5,19 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Nuxt.js + TypeScript + Vuetify + i18n Test" }
+      {
+        hid: "description",
+        name: "description",
+        content: "Nuxt.js + TypeScript + Vuetify + i18n Test"
+      }
     ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   loading: { color: "#3B8070" },
   css: ["~/assets/css/main.css"],
   build: {},
   buildModules: ["@nuxt/typescript-build"],
-  modules: [
-    "@nuxtjs/axios",
-  ],
+  modules: ["@nuxtjs/axios", "nuxt-i18n"],
+  i18n: {},
   axios: {}
-}
+};
